b8a48fbcca0a4644c8565cfbed08915588197400
Celina Louissaint <celinalouissaint@gmail.com>
2021-07-19 16:46:40 -0700

    cleaned up code and added comments

--- client/src/components/AssignMenu.js
+++ client/src/components/AssignMenu.js
@@ -1,33 +1,46 @@
 import { useMutation, useQuery } from '@apollo/client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { UPDATE_TASK } from '../utils/mutations';
 import { QUERY_USERS } from '../utils/queries';
 import { makeStyles } from '@material-ui/core/styles';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-const useStyles = makeStyles((theme) => ({
+import { update } from 'lodash';
 
+const useStyles = makeStyles((theme) => ({
     input: {
-      borderRadius: 4,
-      backgroundColor: theme.palette.background.paper,
-      border: '1px solid #ced4da',
-      fontSize: 16,
-      padding: '5px 26px 5px 12px'
+        borderRadius: 4,
+        backgroundColor: theme.palette.background.paper,
+        border: '1px solid #ced4da',
+        fontSize: 16,
+        padding: '5px 26px 5px 12px',
+        maxWidth: '70px'
     }
-  }));
+}));
 
 
 const AssignMenu = () => {
+    // ----------------------- styles ---------------------------- //
     const classes = useStyles();
-
+    // ---------------------- get user data ---------------------- //
     const { loading, data } = useQuery(QUERY_USERS);
     const users = data?.users || [];
-    const [assign, setAssign] = useState('')
-    // const [updateState] = useMutation(UPDATE_TASK)
 
-    const handleChange = () => {
-        let newStatus = setAssign()
-        // updateState(newStatus)
+    // ----------- set state for who is assigned the task --------- //
+    const [assign, setAssign] = useState('')
+
+    // --------- definie mutation to update assigned to in gql ---------- //
+    const [updateState, { error }] = useMutation(UPDATE_TASK)
+
+
+
+    // ---------------------- handle select's onChange ---------------------- //
+    const handleChange = (e) => {
+
+        // update assigned user based on form input 
+        const assign = e.target.value
+        setAssign(assign)
+        
         console.log(`
         =====================
         OnChange Assigned To
@@ -35,25 +48,40 @@ const AssignMenu = () => {
         Assigned To ${assign}
         =====================
         `)
-    }
-    
+    };
+
+    // ---------------------- Update back end ---------------------- //
+    // this wont work until I can figure out how to get the taskID
+    // useEffect(() => {
+    //     async function updateState() {
+    //         const response = await updateState(assign)
+    //         console.log(`
+    //             =====================
+    //             OnChange Assigned To
+    //             =====================
+    //             Assigned To ${assign}
+    //             =====================
+    //             `)
+    //     }
+    // }, [assign]);
+
     return (
-        <Select
-            labelId="demo-customized-select-label"
-            id="demo-customized-select"
-            // onClick={handleChange}
+        <select
+            labelId="assignSelect"
+            id="assignSelect"
+            onChange={handleChange}
+            value={assign}
             className={classes.input}
-            
         >
-        {
-            users && 
-            users.map(user => (
-                <MenuItem key={user._id} value={user.username}>
-                {user.username}
-                </MenuItem>
-            ))
-        }
-        </Select>
+            {
+                users &&
+                users.map(user => (
+                    <option key={user._id} value={user._id}>
+                        {user.username}
+                    </option>
+                ))
+            }
+        </select>
     )
 }
 
--- client/src/components/Toggler.js
+++ client/src/components/Toggler.js
@@ -1,24 +1,34 @@
 import { useMutation } from '@apollo/client';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { UPDATE_TASK } from '../utils/mutations';
 
 const Toggler = () => {
+     // ----------- set state for task's status the task --------- //
     const [status, setStatus] = useState(false)
+
+    // --------- definie mutation to update status to in gql ---------- //
     // const [updateState] = useMutation(UPDATE_TASK)
 
-    const toggleStatus = () => {
-        let newStatus = setStatus(!status)
-        // updateState(newStatus)
+    // ---------------------- handle onClick ---------------------- //
+    const ToggleStatus = () => {
+        // change state to opposite of what it was
+        setStatus(!status)
     }
-    console.log(`
-      =====================
-      Status Button Cliked
-      =====================
-      Status ${status}
-      =====================
-      `)
+
+    // ---------------------- Update back end ---------------------- //
+    // useEffect(()=> {
+    //     alert('clicked')
+    // })
+    // console.log(`
+    // =====================
+    // Status Button Cliked
+    // =====================
+    // Status ${status}
+    // =====================
+    // `)
+    
     return (
-        <button className="statusBtn" value={status} onClick={toggleStatus}>
+        <button className="statusBtn" value={status} onClick={ToggleStatus}>
             { status ? "👍🏼" : "👎🏼" }
         </button>
     )
